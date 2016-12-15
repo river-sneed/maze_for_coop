9baa5232976bc9610c761c22b310b65f249c9814
River Sneed <microbacondonut@gmail.com>
2016-12-15 15:48:55 -0500

    Add files via upload

--- maze_with_scoring.py
+++ maze_with_scoring.py
@@ -1,14 +1,17 @@
 # Imports
 import pygame
 import intersects
+import random
 
 # Initialize game engine
 pygame.init()
 
 
 # Window
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1250
+HEIGHT = 950
+BLOCK_WIDTH = 50
+BLOCK_HEIGHT = 50
 SIZE = (WIDTH, HEIGHT)
 TITLE = "Maze"
 screen = pygame.display.set_mode(SIZE)
@@ -17,7 +20,7 @@ pygame.display.set_caption(TITLE)
 
 # Timer
 clock = pygame.time.Clock()
-refresh_rate = 60
+refresh_rate = 100
 
 # Colors
 WHITE = (255, 255, 255)
@@ -26,26 +29,67 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+RAINBOW = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 # Make a player
 player =  [200, 150, 25, 25]
 player_vx = 0
 player_vy = 0
-player_speed = 5
+player_speed = 3
+
+
+def rand_color(color):
+    r = random.randint(color[0] - 5, color[0] + 5)
+    b = random.randint(color[1] - 5, color[1] + 5)
+    g = random.randint(color[2] - 5, color[2] + 5)
+
+    if r < 0:
+        r = 0
+    if g < 0:
+        g = 0
+    if b < 0:
+        b = 0
+    if r > 255:
+        r = 255
+    if g > 255:
+        g = 255
+    if b > 255:
+        b = 255
+
+    return(color)
+
+
+
+
+blocks = []
+for x in range(0, WIDTH, 100):
+    for y in range(0, HEIGHT, 100):
+        b = [x, y, BLOCK_WIDTH, BLOCK_HEIGHT]
+        blocks.append(b)
+
+r = random.randint (0, 255)
+g = random.randint (0, 255)
+b = random.randint (0, 255)
+block_color = [r, g, b]
+    
+coins = []
+for x in range(50, (WIDTH-50), 100):
+    for y in range(50, HEIGHT, 100):
+        c = [x, y, BLOCK_WIDTH, BLOCK_HEIGHT]
+        coins.append(c)
+
+        
+    
 
 # make walls
-wall1 =  [300, 275, 200, 25]
-wall2 =  [400, 450, 200, 25]
-wall3 =  [100, 100, 25, 200]
+wall1 =  [0, 0, WIDTH, BLOCK_HEIGHT]
+wall2 =  [0, (HEIGHT-50), WIDTH, BLOCK_HEIGHT]
+wall3 =  [0, 0, 50, WIDTH]
+wall4 =  [(WIDTH-50), 0, BLOCK_WIDTH, HEIGHT]
 
-walls = [wall1, wall2, wall3]
+walls = [wall1, wall2, wall3, wall4]
 
-# Make coins
-coin1 = [300, 500, 25, 25]
-coin2 = [400, 200, 25, 25]
-coin3 = [150, 150, 25, 25]
 
-coins = [coin1, coin2, coin3]
 
 
 # Game loop
@@ -53,10 +97,14 @@ win = False
 score = 0
 
 done = False
-
+ 
 while not done:
     # Event processing (React to key presses, mouse clicks, etc.)
     ''' for now, we'll just check to see if the X is clicked '''
+
+
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -107,6 +155,25 @@ while not done:
                 player[1] = w[1] + w[3]
 
 
+
+    ''' resolve collisions horizontally '''
+    for b in blocks:
+        if intersects.rect_rect(player, b):        
+            if player_vx > 0:
+                player[0] = b[0] - player[2]
+            elif player_vx < 0:
+                player[0] = b[0] + b[2]
+
+    
+    ''' resolve collisions vertically '''
+    for b in blocks:
+        if intersects.rect_rect(player, b):                    
+            if player_vy > 0:
+                player[1] = b[1] - player[3]
+            if player_vy < 0:
+                player[1] = b[1] + b[3]
+
+
     ''' here is where you should resolve player collisions with screen edges '''
 
 
@@ -151,7 +218,10 @@ while not done:
     for c in coins:
         pygame.draw.rect(screen, YELLOW, c)
 
+    for b in blocks:
+        pygame.draw.rect(screen, block_color, b)
 
+    
     # blit score on the screen somewhere
     
     if win:
